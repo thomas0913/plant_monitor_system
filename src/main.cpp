32d691296c17a99f7b398d16b162bd9157d4e091
thomas0913 <408440021@gms.tku.edu.tw>
2023-04-24 14:05:35 +0800

    esp32 httpServer learning

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+const char* ssid = "羊羊ㄉiphone🐑";
+const char* password = "ibmf7777";
+
+WebServer server(80);
+
+void rootRouter();
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("\nIP位置 : ");
+  Serial.println(WiFi.localIP());
+  Serial.print("\n訊號強度 : ");
+  Serial.println(WiFi.RSSI());
+
+  server.on("/", rootRouter);
+  server.on("/about", []() {
+    server.send(200, "text/html; charset=utf-8", "是在哈囉逆?!");
+  });
+  server.onNotFound([]() { //匿名函式
+    server.send(404, "text/plain", "File Not Found!");
+  });
+  server.on("/sw", []() {
+    String state = server.arg("led");
+
+    if (state == "on") {
+      digitalWrite(LED_BUILTIN, LOW);
+    } else if (state = "off") {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    server.send(200, "text/html", "LED is <b>" + state + "</b>.");
+  });
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  server.handleClient();
+}
+
+void rootRouter() {
+  String HTML = "<!DOCTYPE html>\
+  <html><head><meta charset= 'utf-8' ></head>\
+  <body>慢慢長路，總要從第一步開始。\
+  </body></html>";
+  server.send(200, "text/html", HTML);
 }
